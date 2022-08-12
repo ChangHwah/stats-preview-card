96c17e3004807b4cb022d396527f42bc0c5de066
Solomon Swager <zitiswager@gmail.com>
2022-08-12 19:37:33 -0400

    completed desktop view - adding responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,20 @@ module.exports = {
       colors: {
         'very-dark-blue': 'hsl(233, 47%, 7%)',
         'soft-desaturated-blue': 'hsl(244, 38%, 16%)',
-        'soft-violet': 'hsl(277, 64%, 61%)'
-      },
-      backgroundImage: {
-        'image-header-desktop': "url('../images/image-header-desktop.jpg')"
+        'soft-violet': 'hsl(277, 64%, 61%)',
+        'main-paragraph': 'hsla(0, 0%, 100%, 0.75)',
+        'stat-headings': 'hsla(0, 0%, 100%, 0.6)'
       },
       brightness: {
         '35': '.35'
       },
+      fontSize: {
+        'h1': '2.5rem',
+        'h2': '2rem',
+        'p': '1rem'
+      },
       fontFamily: {
-        'body': ['Inter', 'Lexend Deca', ...defaultTheme.fontFamily.sans]
+        'body': ['Inter', '"Lexend Deca"']
       }
     },
   },
